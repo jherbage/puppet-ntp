6a10e6996a958a94ebcb9a06cbe773d25872584b
root <root@jherbage1.mylabserver.com>
2018-01-10 15:47:28 +0000

    first

---
+++ functions/data.pp
@@ -0,0 +1,23 @@
+function ntp::data() {
+  $base_params = {
+    'ntp::service_hasrestart'       => true,
+    'ntp::service_hasstatus'        => true,
+    'ntp::package_name'     => 'ntp',
+    'ntp::package_ensure'   => 'present',
+    'ntp::config_file_mode' => '0664',
+    'ntp::service_ensure'   => 'running',
+    'ntp::service_enable'   => true,
+    'ntp::service_status'   => 'running',
+    'ntp::ntp_servers'          =>  ['0.centos.pool.ntp.org','1.centos.pool.ntp.org'],
+    'ntp::config_name'      => 'ntp.conf',
+  }
+  $service_name = $facts['os']['family'] ? {
+    'Debian'   => 'ntp',
+    'default'  => 'ntpd'
+  }
+ 
+  $extra_params = {
+    'ntp::service_name' => $service_name
+  }
+  $base_params + $extra_params
+}
---
+++ manifests/service.pp
@@ -0,0 +1,14 @@
+class ntp::service(
+  String $service_name        = $ntp::service_name,
+  Boolean $service_enable     = $ntp::service_enable,
+  String $service_ensure      = $ntp::service_ensure,
+  Boolean $service_hasstatus  = $ntp::service_hasstatus,
+  Boolean $service_hasrestart = $ntp::service_hasrestart
+) {
+  service { $service_name:
+    ensure     => $service_ensure,
+    enable     => $service_enable,
+    hasstatus  => $service_hasstatus,
+    hasrestart => $service_hasrestart,
+  }
+}
